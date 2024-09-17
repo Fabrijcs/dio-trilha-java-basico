a32d770d03e6a91ac212136907fadb86400adaef
Fabrijcs <fabriciojcsantos@gmai.com>
2024-09-16 21:26:01 -0300

    smartTv

---
+++ tipo-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,24 @@
+public class TipoVariaveis {
+    public static void main(String[] args) throws Exception {
+
+//tipos primitivos
+//estudem a classe string que representa texto na aplicação
+
+        String meuNome = "Fabricio Jose";
+
+
+        double salarioMinimo = 2.5;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+      
+    }
+}
